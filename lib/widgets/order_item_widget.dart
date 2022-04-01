ef3c494af28537c45986d40946f7060cfb78620b
Mohammad AL-Qannas <44414204+mzmq@users.noreply.github.com>
2022-04-01 09:05:03 +0300

    Finish All Orders page

--- lib/widgets/order_item_widget.dart
+++ lib/widgets/order_item_widget.dart
@@ -2,27 +2,91 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../providers/orders.dart';
-class ItemOrderWidget extends StatelessWidget {
-  final OrderItem order ;
-  const ItemOrderWidget({Key? key , required this.order}) : super(key: key);
+
+class ItemOrderWidget extends StatefulWidget {
+  final OrderItem order;
+
+  const ItemOrderWidget({Key? key, required this.order}) : super(key: key);
+
+  @override
+  State<ItemOrderWidget> createState() => _ItemOrderWidgetState();
+}
+
+class _ItemOrderWidgetState extends State<ItemOrderWidget> {
+  bool _expand = false;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Card(
-        elevation: 5 ,
-        child: ListTile(
-          title: Text('${order.amount} JOD '),
-          subtitle: Row(
-            children: [
-              Text(DateFormat('dd/MM/yyyy ' ).format(order.datetime)),
-              Text(DateFormat().add_jm().format(order.datetime) , style: TextStyle(color: Colors.blue),),
-            ],
-          ),
-          trailing: IconButton(onPressed: (){}, icon: Icon(Icons.expand_more)),
-
-
+        elevation: 5,
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('${widget.order.amount} JOD '),
+              subtitle: Row(
+                children: [
+                  Text(DateFormat('dd/MM/yyyy ').format(widget.order.datetime)),
+                  Text(
+                    DateFormat().add_jm().format(widget.order.datetime),
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ],
+              ),
+              trailing: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _expand = !_expand;
+                    });
+                  },
+                  icon: Icon(_expand ? Icons.expand_less : Icons.expand_more)),
+            ),
+            _expand
+                ? Column(
+                    children: widget.order.products
+                        .map((e) => ListTile(
+                              title: Text('${e.title}'),
+                              leading: CircleAvatar(
+                                backgroundImage: NetworkImage(e.imgUrl),
+                              ),
+                              subtitle: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text(DateFormat('dd/MM/yyyy ')
+                                          .format(widget.order.datetime)),
+                                      Text(
+                                        DateFormat()
+                                            .add_jm()
+                                            .format(widget.order.datetime),
+                                        style: TextStyle(color: Colors.blue),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text('Quantity ${e.quantity}X'),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                ],
+                              ),
+                              trailing: Text('${e.price} JOD'),
+                            ))
+                        .toList(),
+                  )
+                : Container(),
+          ],
         ),
       ),
     );
